70977fd173e60a69b63db1f9d5e04b830a317aae
kaicrabb <124805211+kaicrabb@users.noreply.github.com>
2023-04-07 10:54:35 -0500

    Create Employee.java

---
+++ Project2/src/project2/Employee.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package project2;
+
+public abstract class Employee {
+    
+    String employeeName;
+    int employeeId;
+    boolean isWorking;
+    
+    public Employee(String name, int number, boolean employed){
+        this.employeeName = name;
+        this.employeeId = number;
+        this.isWorking = employed;
+    }
+    
+    public String getName(){
+        return employeeName;
+    }
+    
+    public void setName(String name){
+        this.employeeName = name;
+    }
+    
+    public int getEmployeeId(){
+        return employeeId;
+    }
+    
+    public void setEmployeeId(int number){
+        this.employeeId = number;
+    }
+    
+    public boolean isWorking(){
+        return isWorking;
+    }
+    
+    public void setIsWorking(boolean employed){
+        this.isWorking = employed;
+    }
+    
+    
+      
+}
